77e8e0631976397d5e6b95a3f0d3e0290916da84
felipeborgesm <cafeborges@outlook.com>
2023-10-05 11:01:26 -0300

    pagina de produtos feita

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <app-header></app-header>
-
-    <router-outlet></router-outlet>
-  `,
-  // templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
   title = 'ecommerce';
